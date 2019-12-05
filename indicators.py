189170d1e3d38db4ef2ffabdba7cf69827148e45
Michael Beach <43048355+mlbeach98@users.noreply.github.com>
2019-12-05 11:36:41 -0500

    restructuring

--- indicators.py
+++ indicators.py
@@ -1,19 +1,13 @@
-import numpy as np
-import pandas as pd
-
-df = pd.read_csv("AAPL.csv")
-
-# SMA
 def SMA(df, columnName, length, newColName):
     """
     Function creates a simple moving average of the columnName column in
     df for a given length, and adds it back to the df under the name newColName
 
     Inputs:
-        df - pandas dataframe
-        columnName - name of column in df you would like SMA of
-        length - length of moving average
-        newColName - name of new column in df
+        df(dataframe) - pandas dataframe
+        columnName(string) - name of column in df you would like SMA of
+        length(int) - length of moving average
+        newColName(string) - name of new column in df
     Outputs:
         none
     """
@@ -21,11 +15,11 @@ def SMA(df, columnName, length, newColName):
     colWanted = df[columnName]
     newCol = []
 
-    for i in len(colWanted):
+    for i in range(len(colWanted)):
         if i < length - 1:
             newCol.append(0)
         else:
-            currentAvg = sum(colWanted[i-length-1:i])/length
+            currentAvg = sum(colWanted[i-length+1:i+1])/length
             newCol.append(currentAvg)
 
     df[newColName] = newCol
---
+++ test_file.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+from indicators import SMA
+
+df = pd.read_csv("AAPL.csv")
+
+SMA()
